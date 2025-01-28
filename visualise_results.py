d8990b7a9bc1aa788a937be32e4d800df8c992b4
JasperEppink <jasper.eppink@gmail.com>
2025-01-28 18:07:02 +0100

    Visualise results created

---
+++ visualise_results.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# First case
+# Medium colinearity & 100 instruments
+compare_models = ["ElasticNet (BIC)", "ElasticNet (CV)", "Minimax (BIC)", "Minimax (CV)", "Post-Lasso (BIC)", "Post-Lasso (CV)"]
+
+correlation_values = [0.5, 0.9]
+n_samples = 100
+n_instruments = 100
+
+fig, axs = plt.subplots(1, 2, figsize=(14, 7), sharey=True)
+
+for i, correlation in enumerate(correlation_values):
+    result_df = pd.DataFrame()
+    for concentration in [30, 180]:
+        for model in compare_models:
+            data = pd.read_csv(f"results/{model}.csv")
+            data_filtered = data[(data["Correlation"] == correlation) & (data["N instruments"] == n_instruments) & (data["Concentration"] == concentration) & (data["N samples"] == n_samples)].copy()
+            data_filtered.loc[:, "Concentration"] = concentration
+            result_df = pd.concat([result_df, data_filtered])
+    result_df = result_df.reset_index(drop=True)
+    
+    # Create bar chart
+    bar_width = 0.35
+    index = np.arange(len(compare_models))
+    colors = ['b', 'g']
+    
+    for j, concentration in enumerate([30, 180]):
+        means = result_df[result_df["Concentration"] == concentration].groupby('Model')['MAD'].mean().reindex(compare_models)
+        stds = result_df[result_df["Concentration"] == concentration].groupby('Model')['MAD'].std().reindex(compare_models)
+        
+        axs[i].bar(index + j * bar_width, means, bar_width, yerr=stds, capsize=5, label=f'Concentration = {concentration}', color=colors[j])
+    
+    axs[i].set_xlabel('Models')
+    axs[i].set_title(f'Correlation = {correlation}')
+    axs[i].set_xticks(index + bar_width / 2)
+    axs[i].set_xticklabels(compare_models, rotation=45, ha='right')
+
+axs[0].set_ylabel('Mean Absolute Deviation (MAD)')
+axs[0].legend()
+fig.suptitle(f'Comparison of Models - N samples = {n_samples}, N instruments = {n_instruments}', fontsize=16)
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+plt.savefig(f"plots/Comparison of Models - N samples = {n_samples}, N instruments = {n_instruments}.png")
+plt.show()
